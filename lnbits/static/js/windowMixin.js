6449276003001113991b4195fb8621bc43f0a9ab
dni ⚡ <office@dnilabs.com>
2025-11-28 17:58:50 +0100

    feat: move qrcode scanner into reuseable component (#3567)
    Co-authored-by: Tiago Vasconcelos <talvasconcelos@gmail.com>

--- lnbits/static/js/windowMixin.js
+++ lnbits/static/js/windowMixin.js
@@ -1,5 +1,15 @@
 window.windowMixin = {
   methods: {
+    handleScan(val) {
+      if (this.g.scanDetectCallback) {
+        this.g.scanDetectCallback(val)
+        this.g.scanDetectCallback = null
+      }
+    },
+    openScanDialog(scanDetectCallback) {
+      this.g.showScanner = true
+      this.g.scanDetectCallback = scanDetectCallback
+    },
     openNewWalletDialog(walletType = 'lightning') {
       this.g.newWalletType = walletType
       this.g.showNewWalletDialog = true
